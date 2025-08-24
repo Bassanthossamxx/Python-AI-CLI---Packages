024b4d5526e089e4506cf6444a0acfc48be723a4
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-08-24 23:24:48 +0300

    refactor main.py to streamline GitPython usage and remove commented-out code

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-# from git import Repo
-#  #To check repo name
-# repo = Repo('.')
-# print(repo) # prints the repo name and place in your system
-# print(repo.git.status()) # prints the status of the repo and details
-# print(repo.git.branch()) # prints all branches name
-# print(repo.active_branch.name) # prints the active branch name
-# # list 2 commits in the active branch
-# commits = list(repo.iter_commits(repo.active_branch.name))
-# for commit in commits:
-#     print(commit.message + "  "+ commit.author.name)
-# print(repo.git.diff("--cached"))
+from git import Repo
+repo = Repo('.')
+print(repo) # prints the repo name and place in your system
+print(repo.git.diff("--cached"))
 # import sys
 # print(sys.argv)
-import sys
-if len(sys.argv)<3:
-    print("pleas if you want to commit use -m flag")
-else:
-     if sys.argv[2]=='-m' and len(sys.argv)>=4:
-         message = " ".join(sys.argv[3:])
-         print("commit message is: ",message)
-     else:
-          print( "invalid flag or commit message missing")
-
+# import sys
+# if len(sys.argv)<3:
+#     print("pleas if you want to commit use -m flag")
+# else:
+#      if sys.argv[2]=='-m' and len(sys.argv)>=4:
+#          message = " ".join(sys.argv[3:])
+#          print("commit message is: ",message)
+#      else:
+#           print( "invalid flag or commit message missing")
+#
+# #s
